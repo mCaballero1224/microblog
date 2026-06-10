9184c02911499319b4702ce5e90d3323f359b8a8
mCaballero1224 <mcaballero.dev@gmail.com>
2026-06-10 15:14:55 +0900

    create app; start work on db support

---
+++ app/models.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+
+from app import db
+
+
+class User(db.Model):
+    """
+    Class to represent a user for Microblog
+    Makes use of SQLAlchemy to translate class into a Relational DB row.
+    """
+
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+
+    def __repr__(self):
+        return "<User {}>".format(self.username)
