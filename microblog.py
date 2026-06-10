a42f9bc59784e53b52fd02775a8233c59f2ba059
mCaballero1224 <mcaballero.dev@gmail.com>
2026-06-10 17:12:47 +0900

    use shell context processor function to handle imports when testing

--- microblog.py
+++ microblog.py
@@ -1,1 +1,17 @@
-from app import app
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+
+from app import app, db
+from app.models import Post, User
+
+
+@app.shell_context_processor
+def make_shell_context():
+    """
+    Registered shell context function. When `flask shell` is run, this function
+    is invoked and registers the items with the dictionary returned.
+
+    This saves you time and headache with having to import everything when working
+    in the shell for testing purposes.
+    """
+    return {"sa": sa, "so": so, "db": db, "User": User, "Post": Post}
